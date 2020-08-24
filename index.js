3b2a376075144e8ad183579a72400d199b079a71
tutyamxx <tutygoo@gmail.com>
2020-08-24 15:53:28 +0100

    Added a fancy npm logo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  *  check-custom-url - Simple NPM package to check a string for a valid custom filtered URL!
- *  @version: v1.1.6
+ *  @version: v1.1.7
  *  @link: https://github.com/tutyamxx/custom-url-check
  *  @license: MIT
  **/
