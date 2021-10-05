f147e0595ca04148e68b4302760b63ffa19b7d49
Alexandru Maxim <15928925+tutyamxx@users.noreply.github.com>
2021-10-05 18:35:31 +0100

    FIX: Code tweaks and more tests

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 /**
  *  check-custom-url - Simple NPM package to check a string for a valid custom filtered URL!
- *  @version: v1.2.2
+ *  @version: v1.2.3
  *  @link: https://github.com/tutyamxx/custom-url-check
  *  @license: MIT
  **/
 
 module.exports = (url, filterParam) => {
-    if (url.length <= 0 || !url || typeof url !== "string") return false;
-    if (!filterParam || typeof filterParam !== "string") throw "@filter parameter cannot be empty!";
+    if (!url) throw "@url parameter cannot be empty!";
+    else if (typeof url !== "string") throw "@url parameter must be a string!";
+
+    if (!filterParam) throw "@filter parameter cannot be empty!";
+    else if (typeof filterParam !== "string") throw "@filter parameter must be a string!";
 
     const providedURL = url.trim();
-    const regexCheckCustomURL = new RegExp("^(?:https?:\/\/)?(?:www\.)?" + filterParam.toString() + "\.[-=;:?!#@$&_~'+,a-zA-Z0-9./]+$", "gi");
+    const regexCheckCustomUrl = new RegExp("^(?:https?:\/\/)?(?:www\.)?" + filterParam?.toString() + "\.[-=;:?!#@$&_~'+,a-zA-Z0-9./]+$", "gi");
 
-    return providedURL.match(regexCheckCustomURL) ? true : false;
+    return providedURL.match(regexCheckCustomUrl) ? true : false;
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-const customURL = require("../index.js");
+const customUrl = require("../index.js");
 
 // --| As an example we are going to check for valid YouTube URL's
 const validYoutubeLinks = [
@@ -20,33 +20,48 @@ const invalidYoutubeLinks = [
 
 test("Check for valid custom YouTube links", () => {
     validYoutubeLinks.forEach((url) => {
-        expect(customURL(url, "YouTube")).toBe(true);
+        expect(customUrl(url, "YouTube")).toBe(true);
     });
 });
 
 test("Check for valid custom YouTube links case insensitive", () => {
     validYoutubeLinks.forEach((url) => {
-        expect(customURL(url, "youTUBE")).toBe(true);
-        expect(customURL(url, "YoutubE")).toBe(true);
-        expect(customURL(url, "YOUTUBE")).toBe(true);
+        expect(customUrl(url, "youTUBE")).toBe(true);
+        expect(customUrl(url, "YoutubE")).toBe(true);
+        expect(customUrl(url, "YOUTUBE")).toBe(true);
     });
 });
 
 test("Check for invalid custom YouTube links", () => {
     invalidYoutubeLinks.forEach((url) => {
-        expect(customURL(url, "YouTube")).toBe(false);
+        expect(customUrl(url, "YouTube")).toBe(false);
     });
 });
 
 test("Check for invalid custom YouTube links case insensitive", () => {
     invalidYoutubeLinks.forEach((url) => {
-        expect(customURL(url, "YouTube")).toBe(false);
-        expect(customURL(url, "YoutubE")).toBe(false);
-        expect(customURL(url, "YOUTUBE")).toBe(false);
+        expect(customUrl(url, "YouTube")).toBe(false);
+        expect(customUrl(url, "YoutubE")).toBe(false);
+        expect(customUrl(url, "YOUTUBE")).toBe(false);
     });
 });
 
-test("Should throw an error if filter param is empty", () => {
-    expect(() => { customURL("https://www.youtube.com/", "") }).toThrowError("@filter parameter cannot be empty!");
-    expect(() => { customURL("https://www.youtube.com/") }).toThrowError("@filter parameter cannot be empty!");
+test("Should throw an error if url param is empty or not specified", () => {
+    expect(() => { customUrl("") }).toThrowError("@url parameter cannot be empty!");
+    expect(() => { customUrl() }).toThrowError("@url parameter cannot be empty!");
+});
+
+test("Should throw an error if filter param is empty or not specified", () => {
+    expect(() => { customUrl("https://www.youtube.com/", "") }).toThrowError("@filter parameter cannot be empty!");
+    expect(() => { customUrl("https://www.youtube.com/") }).toThrowError("@filter parameter cannot be empty!");
+});
+
+test("Should throw an error if url param is not a string", () => {
+    expect(() => { customUrl(1234) }).toThrowError("@url parameter must be a string!");
+    expect(() => { customUrl(-123.54) }).toThrowError("@url parameter must be a string!");
+});
+
+test("Should throw an error if filter param is not a string", () => {
+    expect(() => { customUrl("https://www.youtube.com/", 1234) }).toThrowError("@filter parameter must be a string!");
+    expect(() => { customUrl("https://www.youtube.com/", -123.54) }).toThrowError("@filter parameter must be a string!");
 });
