58b9aec3c97d27c3829c2f97f9b4d7461827a951
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2022-05-22 16:25:30 +0100

    UPDATE: Slight code tweak

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  *  check-custom-url - Simple NPM package to check a string for a valid custom filtered URL!
- *  @version: v1.2.5
+ *  @version: v1.2.6
  *  @link: https://github.com/tutyamxx/custom-url-check
  *  @license: MIT
  **/
@@ -13,7 +13,7 @@ module.exports = (url, filterParam) => {
     else if (typeof filterParam !== 'string') throw new Error('@filter parameter must be a string!');
 
     const providedURL = url?.trim();
-    const regexCheckCustomUrl = new RegExp('^(?:https?:\/\/)?(?:www\.)?' + filterParam.toString() + '\.[-=;:?!#@$&_~\'+,a-zA-Z0-9./]+$', 'gi');
+    const regexCheckCustomUrl = new RegExp('^(?:https?:\/\/)?(?:www\.)?' + filterParam?.toString() + '\.[-=;:?!#@$&_~\'+,a-zA-Z0-9./]+$', 'gi');
 
     return !!providedURL?.match(regexCheckCustomUrl);
 };
