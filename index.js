4129e97a15f0a0e35584b1095869118d8f1b53fb
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2026-03-04 13:59:45 +0000

    UPDATE: Updated dependencies and CI file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  *  check-custom-url - Simple NPM package to check a string for a valid custom filtered URL!
- *  @version: v1.4.3
+ *  @version: v1.4.4
  *  @link: https://github.com/tutyamxx/custom-url-check
  *  @license: MIT
  **/
